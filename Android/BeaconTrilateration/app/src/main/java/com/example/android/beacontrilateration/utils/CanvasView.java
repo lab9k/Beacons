8206c8a9a81c5b578c44a2ef029f3d365eac6805
lotteje <lotje.j@hotmail.com>
2018-04-10 16:14:48 +0200

    zones detecteren

--- Android/BeaconTrilateration/app/src/main/java/com/example/android/beacontrilateration/utils/CanvasView.java
+++ Android/BeaconTrilateration/app/src/main/java/com/example/android/beacontrilateration/utils/CanvasView.java
@@ -52,44 +52,6 @@ public class CanvasView extends View {
         //kleine afbeelding
         canvas.drawCircle((float) (locationX / 193.9653543) * width, (float) (locationY / 147.9307087) * height, 3, locationPaint);
 
-
-//        canvas.rotate((float) -19.27, (float) 245.6692913, (float) 113.3858268);
-////        // Zone 1
-////        canvas.rotate((float) -18.19, (float) (100.119685 / 491.3385827) * width, (float) (203.111811 / 226.7716535) * height);
-//        canvas.drawRect((float) (45.35433071 / 491.3385827) * width, (float) (171.3259843 / 226.7716535) * height, (float) (100.119685 / 491.3385827) * width, (float) (203.111811 / 226.7716535) * height, locationPaint);
-//        canvas.save();
-//        canvas.restore();
-//
-////        // Zone 2
-////        canvas.rotate((float) -19.27, (float) (36.01889764 / 491.3385827) * width, (float) (138.3307087 / 226.7716535) * height);
-//        canvas.drawRect((float) (36.01889764 / 491.3385827) * width, (float) (138.3307087 / 226.7716535) * height, (float) (73.20944882 / 491.3385827) * width, (float) (172.119685 / 226.7716535) * height, locationPaint);
-//        canvas.save();
-//        canvas.restore();
-//
-//        // Zone 3
-////        canvas.rotate((float) -19.27, (float) (20.37165354  / 491.3385827) * width, (float) (95.81102362 / 226.7716535) * height);
-//        canvas.drawRect((float) (20.37165354 / 491.3385827) * width, (float) (95.81102362 / 226.7716535) * height, (float) (54.38740157 / 491.3385827) * width, (float) (136.1385827 / 226.7716535) * height, locationPaint);
-//        canvas.save();
-//        canvas.restore();
-////
-//////        // Zone 4
-////        canvas.rotate((float) -19.27, (float) (101.6314961 / 491.3385827) * width, (float) (112.2897638 / 226.7716535) * height);
-//        canvas.drawRect((float) (53.21574803 / 491.3385827) * width, (float) (81.37322835 / 226.7716535) * height, (float) (101.6314961 / 491.3385827) * width, (float) (112.2897638 / 226.7716535) * height, locationPaint);
-//        canvas.save();
-//        canvas.restore();
-////
-//////        // Zone 5
-////        canvas.rotate((float) -19.27, (float) (163.3133858 / 491.3385827) * width, (float) (92.14488189 / 226.7716535) * height);
-//        canvas.drawRect((float) (102.6141732 / 491.3385827) * width, (float) (61.68188976 / 226.7716535) * height, (float) (163.3133858 / 491.3385827) * width, (float) (92.14488189 / 226.7716535) * height, locationPaint);
-//        canvas.save();
-//        canvas.restore();
-////
-//////        // Zone 6
-////        canvas.rotate((float) -19.27, (float) (185.4992126 / 491.3385827) * width, (float) (77.51811024 / 226.7716535) * height);
-//        canvas.drawRect((float) (163.2 / 491.3385827) * width, (float) (47.01732283 / 226.7716535) * height, (float) (185.4992126 / 491.3385827) * width, (float) (77.51811024 / 226.7716535) * height, locationPaint);
-//        canvas.save();
-
-
     }
 
 
--- Android/BeaconTrilateration/app/src/main/java/com/example/android/beacontrilateration/utils/Kalman.java
+++ Android/BeaconTrilateration/app/src/main/java/com/example/android/beacontrilateration/utils/Kalman.java
@@ -3,42 +3,127 @@ package com.example.android.beacontrilateration.utils;
 
 import java.util.ArrayList;
 
+import static java.lang.Double.NaN;
+
 /**
  * Created by lottejespers.
  */
 public class Kalman {
 
-    /* Complete calculation of Kalman Filter */
-    public static Double kalman(ArrayList<Double> inputValues, double initialVariance, double noise) {
-        return calculate(inputValues, initialVariance, noise);
+//    /* Complete calculation of Kalman Filter */
+//    public static Double kalman(ArrayList<Double> inputValues, double initialVariance, double noise) {
+//        return calculate(inputValues, initialVariance, noise);
+//    }
+//
+//    /* Calculation of Kalman Filter using default values for wireless Access Points data acquisition */
+//    public static Double kalman(ArrayList<Double> inputValues) {
+//        return calculate(inputValues, 50.0, 0.008);
+//    }
+//
+//    /* Calculation of arithmetic mean */
+//    public static Double mean(ArrayList<Double> inputValues) {
+//        return StatUtils.mean(inputValues);
+//    }
+//
+//
+//    /*This method is the responsible for calculating the value refined with Kalman Filter */
+//    private static Double calculate(ArrayList<Double> inputValues, double initialVariance, double noise) {
+//        Double kalmanGain;
+//        Double variance = initialVariance;
+//        Double processNoise = noise;
+//        Double measurementNoise = StatUtils.variance(inputValues);
+//        Double mean = inputValues.get(0);
+//
+//        for (Double value : inputValues) {
+//            variance = variance + processNoise;
+//            kalmanGain = variance / ((variance + measurementNoise));
+//            mean = mean + kalmanGain * (value - mean);
+//            variance = variance - (kalmanGain * variance);
+//        }
+//
+//        return mean;
+//    }
+
+
+
+/* translated a Javascript KalmanFilter : https://github.com/wouterbulten/kalmanjs */
+
+    private double r;
+    private double q;
+    private double a;
+    private double b;
+    private double c;
+    private double cov;
+    private double x;
+
+    public Kalman(double r, double q, double a, double b, double c) {
+        this.r = (r == 0) ? 1 : r;
+        this.q = (q == 0) ? 1 : q;
+        this.a = (a == 0) ? 1 : a;
+        this.b = b;
+        this.c = (c == 0) ? 1 : c;
+        this.cov = NaN;
+        this.x = NaN;
     }
 
-    /* Calculation of Kalman Filter using default values for wireless Access Points data acquisition */
-    public static Double kalman(ArrayList<Double> inputValues) {
-        return calculate(inputValues, 50.0, 0.008);
-    }
+    public double filter(double z, double u) {
+        if (this.x == NaN) {
+            x = (1 / c) * z;
+            cov = (1 / c) * q * (1 / c);
+        } else {
+            // prediction
+            double predX = this.predict(u);
+            double predCov = this.uncertainty();
+            // Kalman
+            double k = predCov * c * (1 / ((c * predCov * c) + q));
+            // Correction
+            x = predX + k * (z - (c * predX));
+            cov = predCov - (k * c * predCov);
 
-    /* Calculation of arithmetic mean */
-    public static Double mean(ArrayList<Double> inputValues) {
-        return StatUtils.mean(inputValues);
-    }
-
-
-    /*This method is the responsible for calculating the value refined with Kalman Filter */
-    private static Double calculate(ArrayList<Double> inputValues, double initialVariance, double noise) {
-        Double kalmanGain;
-        Double variance = initialVariance;
-        Double processNoise = noise;
-        Double measurementNoise = StatUtils.variance(inputValues);
-        Double mean = inputValues.get(0);
-
-        for (Double value : inputValues) {
-            variance = variance + processNoise;
-            kalmanGain = variance / ((variance + measurementNoise));
-            mean = mean + kalmanGain * (value - mean);
-            variance = variance - (kalmanGain * variance);
         }
-
-        return mean;
+        return x;
     }
+
+    public double predict(double u) {
+        return (a * x) + (b * u);
+    }
+
+    public double uncertainty() {
+        return ((a * cov) * a) + r;
+    }
+
+    public double lastMeasurement() {
+        return x;
+    }
+
+    public void setMeasurementNoise(double noise) {
+        q = noise;
+    }
+
+    public void setProcessNoise(double noise) {
+        r = noise;
+    }
+
+
+    // example :
+//    let random = GKRandomSource()
+//    let gaussian = GKGaussianDistribution(randomSource:random,mean:0,deviation:3)
+    // simple dataset :
+//    var dataSet =[Double](repeating:4.0,count:20)
+    // adding noise :
+//    var noisyDataSet = dataSet.map({
+//            (value:Double)->
+//    Double in
+//        return value+
+//
+//    Double(gaussian.nextUniform());
+//})
+// apply kalman filter :
+
+
+//        var kalmanFilterData=noisyDataSet.map({
+//        (value:Double)->Double in
+//        return kalmanFilter.filter(z:value);
+//        })
+
 }
